eba095f47c71c56636fdb93cbe1ac80c6700387c
S10203927-LowHongWei <S10203927@connect.np.edu.sg>
2021-11-30 11:32:41 +0800

    Half way done with the website

---
+++ Shopping Cart.js
@@ -0,0 +1,10 @@
+let productsincart = [];
+const parentElement = document.querySelector("#buyItems")
+const cartSumPrice = Document.querySelector("#sum-prices")
+const products = document.querySelectorAll('.product-card')
+
+products.forEach(products => {
+    products.addEventListener('click', (e) => {
+        if(e.target.contains)
+    })
+})
