e39821397cb4bfc59843a42bc79a5a2d32633bbd
S10203927-LowHongWei <S10203927@connect.np.edu.sg>
2021-12-04 01:39:11 +0800

    Final Version

--- AddingtoCart.js
+++ AddingtoCart.js
@@ -6,7 +6,7 @@ ProductPrice*/
 let CartList = []
 
 if(document.readyState == 'loading'){
-    document.addEventListener('DOMContentLoaded',ready) //checks if page has loaded finish
+    document.addEventListener('DOMContentLoaded',ready) //checks if page has loaded finish if it has it will call the ready function
 } else{
     ready()
 }
@@ -15,18 +15,18 @@ function ready(){
     var removeCartItemButton = document.getElementsByClassName('ProductRemove')
     for (var i = 0; i<removeCartItemButton.length;i++){
         var button = removeCartItemButton[i]
-        button.addEventListener('click',removeCartItem)
+        button.addEventListener('click',removeCartItem)//when ProductRemove button has been clicked it will fire the removeCartItem function
     }
 
     var quantityInputs = document.getElementsByClassName('CartQuantity')
     for (var i = 0; i <quantityInputs.length;i++){
         var input = quantityInputs[i]
-        input.addEventListener('change',quantityChanged)
+        input.addEventListener('change',quantityChanged)//when CartQuantity button has been changed it will fire the quantityChanged function
     }
     var addToCartButtons = document.getElementsByClassName('ProductButton')
     for (var i = 0; i <addToCartButtons.length;i++){
         var button = addToCartButtons[i]
-        button.addEventListener('click', addingToArray)
+        button.addEventListener('click', addingToArray)//when ProductButton button has been clicked it will fire the addingToArray function
     }
 }
 
@@ -38,17 +38,17 @@ function addingToArray(event){
         price: product.getElementsByClassName('ProductPrice')[0].innerText,
         img : product.getElementsByClassName('ProductImage')[0].src
     }
-        var existingstring = localStorage.getItem('MyCartList')
-        if (existingstring == null){
-            CartList.push(addcartitems);
+        var existingstring = localStorage.getItem('MyCartList')//makes a var from exisiting array
+        if (existingstring == null){//if there is no array exisiting it will create one
+            CartList.push(addcartitems);//adds the new object to the array
             console.warn('added',{CartList});
             alert('Item added to cart')
-            localStorage.setItem('MyCartList',JSON.stringify(CartList)); 
+            localStorage.setItem('MyCartList',JSON.stringify(CartList)); //makes it string to transfer over to other pages from localStorage
         }else{
-            var existing = JSON.parse(existingstring);
-            existing.push(addcartitems);
+            var existing = JSON.parse(existingstring);//if there is exisiting array it will convert it back from string to array
+            existing.push(addcartitems);//adds new object
             console.warn('added',{existing});
             alert('Item added to cart')
-            localStorage.setItem('MyCartList',JSON.stringify(existing)); 
+            localStorage.setItem('MyCartList',JSON.stringify(existing)); //makes array into string for localStorage
         }
 };
--- Shopping Cart.js
+++ Shopping Cart.js
@@ -1,8 +1,8 @@
-var localstring = localStorage.getItem('MyCartList');
-var ItemsinArray = JSON.parse(localstring);
+var localstring = localStorage.getItem('MyCartList');//gets array
+var ItemsinArray = JSON.parse(localstring);//converts string to array
 console.log(ItemsinArray);
 
-let CartList = []
+let CartList = []//empty array list
 
 if(document.readyState == 'loading'){
     document.addEventListener('DOMContentLoaded',ready) //checks if page has loaded finish
@@ -14,22 +14,22 @@ if(document.readyState == 'loading'){
 
 function addToCartClicked(event){
     for (var i = 0; i <ItemsinArray.length; i++){
-    var title = ItemsinArray[i].title
-    var price = ItemsinArray[i].price
-    var img = ItemsinArray[i].img
+    var title = ItemsinArray[i].title//gets that items title
+    var price = ItemsinArray[i].price//gets that items price
+    var img = ItemsinArray[i].img// gets that items image link
     console.log(title,price,img)
     if (title)
-    addItemToCart(title,price,img)
-    updateCartTotal()
+    addItemToCart(title,price,img)//calls function and puts perimeters inside
+    updateCartTotal()//updates cart total because it added a new items so there is a new total
     }
 }
 
-function addItemToCart(title,price,imageSrc){
+function addItemToCart(title,price,imageSrc){//adds that item to cart
     var cartRow = document.createElement('div')
     cartRow.classList.add('CartRow')
     var cartItems = document.getElementsByClassName('CartItems')[0]
     var cartItemNames =cartItems.getElementsByClassName('CartTitle')
-    for (var i = 0; i <cartItemNames.length; i++){
+    for (var i = 0; i <cartItemNames.length; i++){//checks if there are duplicates
         if (cartItemNames[i].innerHTML == title){
             alert('There has been a duplicate item added!')
             return
@@ -44,40 +44,40 @@ function addItemToCart(title,price,imageSrc){
             <div class="CartColumn">
                 <input class="CartQuantity" type="number" value="1">
                 <button class="ProductRemove" type="button">REMOVE</button>
-            </div>`
+            </div>`//prints out the items in that format
         cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
-    cartRow.getElementsByClassName('ProductRemove')[0].addEventListener('click',removeCartItem)
-    cartRow.getElementsByClassName('CartQuantity')[0].addEventListener('change',quantityChanged)
+    cartRow.getElementsByClassName('ProductRemove')[0].addEventListener('click',removeCartItem)//if they click on the remove button it will call the remove function which will remove that item
+    cartRow.getElementsByClassName('CartQuantity')[0].addEventListener('change',quantityChanged)//sees if quantity changed so they make sure it cant go below 1 and can update cart total
 }
 
 function removeCartItem(event){
     var buttonclicked = event.target
-    buttonclicked.parentElement.parentElement.remove()
-    updateCartTotal()
+    buttonclicked.parentElement.parentElement.remove()//removes that item in the array
+    updateCartTotal()//updates price
 }
 
 function quantityChanged(event){
     var input = event.target
-    if (isNaN(input.value) || input.value <=0){
+    if (isNaN(input.value) || input.value <=0){//makes it so there cant be below 1 quantity
         input.value=1
     }
-    updateCartTotal()
+    updateCartTotal()//updates quantity change
 }
 
- function updateCartTotal(){
-     var cartItemContainer =document.getElementsByClassName('CartItems')[0]
-     var CartRows = cartItemContainer.getElementsByClassName('CartRow')
+ function updateCartTotal(){//Updates total
+     var cartItemContainer =document.getElementsByClassName('CartItems')[0]//wants the first element of the array
+     var CartRows = cartItemContainer.getElementsByClassName('CartRow')//gets all the elements inside the object like price or title
      var total=0
     for (var i = 0; i<CartRows.length;i++){
         var CartRow = CartRows[i]
-        var PriceElement = CartRow.getElementsByClassName('CartPrice')[0]
-        var quantityElement =CartRow.getElementsByClassName('CartQuantity')[0]
+        var PriceElement = CartRow.getElementsByClassName('CartPrice')[0]//gets price
+        var quantityElement =CartRow.getElementsByClassName('CartQuantity')[0]//gets quantity
         var price = parseFloat(PriceElement.innerText.replace('$',''))  //parsefloat turns any string into a float
         var quantity = quantityElement.value
-        total =total+(price*quantity)
-    }total = Math.round((total*100) / 100 *0.85)
-    document.getElementsByClassName('CartTotal')[0].innerText = "$" + total
+        total =total+(price*quantity)//finds total
+    }total = Math.round((total*100) / 100 *0.85)//makes the total 2 decimal
+    document.getElementsByClassName('CartTotal')[0].innerText = "$" + total//shows the total
  }
 
  function ready(){
@@ -97,12 +97,12 @@ function quantityChanged(event){
         var button = addToCartButtons[i]
         button.addEventListener('click', addingToArray)
     }
-    document.getElementsByClassName('PurchaseButton')[0].addEventListener('click',CartClicked)
+    document.getElementsByClassName('PurchaseButton')[0].addEventListener('click',CartClicked)//when the PurchaseButton is clicked it will call CartClicked function
 }
 
 function CartClicked(){
-    alert('Please enter your card details to pay!')
-    window.localStorage.clear();
-    location.reload()
-    updateCartTotal()
-}
+    alert('Please enter your card details to pay!')//sends a message to the page where it pops up
+    window.localStorage.clear();//clears the local storage as they are now paying
+    location.reload()//reloads the whole page so the page updates
+    updateCartTotal()//updates the total as there is no items
+} 
